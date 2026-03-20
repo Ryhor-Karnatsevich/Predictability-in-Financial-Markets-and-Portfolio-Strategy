3c72df08c605ab5164a00230ed543d1ed66ba194
Ryhor-Karnatsevich <spamjamesmorgan@gmail.com>
2026-03-20 12:38:48 +0100

    code is ready

--- src/Data_prep/Data cleaning.py
+++ src/Data_prep/Data cleaning.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv(r"../../Data/all_stocks.csv")
+df = pd.read_csv(r"../../Data/test_data.csv")
 
 dft = df.groupby("Ticker").agg(
-    close = ('Close','mean')
+    close = ('Close','mean'),
+    open = ('Open','mean')
 ).reset_index()
 
 dft['close'] = round(dft['close'],2)
+dft['open'] = round(dft['open'],2)
 
 print(dft.head())
--- src/Data_prep/test data.py
+++ src/Data_prep/test data.py
@@ -2,6 +2,12 @@ import pandas as pd
 
 df = pd.read_csv(r"../../Data/all_stocks.csv")
 df['Date'] = pd.to_datetime(df["Date"])
-#print(df.head())
+
+export_path = r"..\..\Data\test_data.csv"
+
+print(df.info())
+print(df.head())
+print(df.tail())
+
 filter = df[df["Date"].dt.year >= 2019].reset_index(drop=True)
-filter.to_csv(r"Data_prep\test.csv")
+filter.to_csv(export_path, index=False)
